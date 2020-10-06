2b26a903b30983d46b22333ce4498c5d5d511cb0
PRIYAL898 <71829008+PRIYAL898@users.noreply.github.com>
2020-10-06 18:16:53 +0530

    Add files via upload

---
+++ sketch 2.js
@@ -0,0 +1,42 @@
+var speed,weight,car,wall,deformation;
+
+function setup() {
+  createCanvas(800,400);
+  car=createSprite(50, 200, 50, 50);
+  car.shapeColor="white";
+
+  wall=createSprite(700,140,40,600);
+  wall.shapeColor="black";
+
+  speed=random(55,90);
+  weight=random(400,4000);
+
+  car.velocityX=speed;
+}
+
+function draw() {
+  background("lightBlue"); 
+
+  console.log(car.velocityX);
+
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0;
+  
+    var deformation=0.5*weight*speed*speed/22500;
+    car.shapeColor="green";
+    if(deformation<100){
+      car.shapeColor="green";
+    }
+
+    else if(deformation>100 && deformation<180){
+        car.shapeColor="yellow";
+    }
+
+    else if(deformation>180){
+        car.shapeColor="red";
+    }
+  }
+   
+  
+  drawSprites();
+}
